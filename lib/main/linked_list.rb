89798dd88502e634cb6668c543c96dd530b71133
ElectronDragon <electricdragondevelopment@gmail.com>
2024-06-29 08:18:42 +0100

    Create skeleton functions with comments in LinkedList

--- lib/main/linked_list.rb
+++ lib/main/linked_list.rb
@@ -1,4 +1,45 @@
 class LinkedList
   def initialize
     @head = nil
+  end
+
+  def append(value)
+    # adds a new node containing value to the end of the list
+  end
+
+  def prepend(value)
+    # adds a new node containing value to the start of the list
+  end
+
+  def size
+    # returns the total number of nodes in the list
+  end
+
+  def head
+    #  returns the first node in the list
+  end
+
+  # returns the last node in the list
+  end
+
+  def at(index)
+    # returns the node at the given index
+  end
+
+  def pop
+    #  removes the last element from the list
+  end
+
+  def contains?(value)
+    #  returns true if the passed in value is in the list and otherwise returns false. returns true if the passed in value is in the list and otherwise returns false.
+  end
+
+  def find(value)
+    # returns the index of the node containing value, or nil if not found.
+  end
+
+  def to_s
+    # represent your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> nil
+  end
+
 end
